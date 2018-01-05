36716ead4ed4e44ebee84aef37de477522133852
MilesChou <jangconan@gmail.com>
2018-01-05 22:18:23 +0800

    調整 Dusk 測試

--- tests/Browser/ExampleTest.php
+++ tests/Browser/ExampleTest.php
@@ -4,20 +4,41 @@ namespace Tests\Browser;
 
 use Tests\DuskTestCase;
 use Laravel\Dusk\Browser;
-use Illuminate\Foundation\Testing\DatabaseMigrations;
 
 class ExampleTest extends DuskTestCase
 {
     /**
-     * A basic browser test example.
-     *
-     * @return void
+     * @test
      */
-    public function testBasicExample()
+    public function shouldBeOkWhenSeeIndexPage()
     {
         $this->browse(function (Browser $browser) {
             $browser->visit('/')
-                    ->assertSee('Laravel');
+                ->assertSee('產品分類')
+                ->assertSee('管理員頁面')
+                ->assertSee('聯絡我們')
+                ->assertSee('查看購物車')
+                ->assertSee('回首頁')
+                ->clickLink('管理員頁面')
+                ->assertSee('統計功能')
+                ->clickLink('查看購物車')
+                ->assertSee('購物清單');
+        });
+    }
+
+    /**
+     * @test
+     */
+    public function shouldBeOkWhenSeeAdminPage()
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->visit('/admin.php')
+                ->assertSee('管理功能')
+                ->assertSee('統計功能')
+                ->clickLink('商品管理')
+                ->assertSee('庫存')
+                ->clickLink('訂單管理')
+                ->assertSee('訂單列表');
         });
     }
 }
